28a12af05eb783b113d9c1a1bdbc383c236809ae
odd <1161465+oddsun@users.noreply.github.com>
2021-01-18 22:16:28 -0500

    +renderOption -> userSelector, fixed filterOptions

--- src/UserSelector.tsx
+++ src/UserSelector.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete, {
+  createFilterOptions,
+} from "@material-ui/lab/Autocomplete";
 
 const filter = createFilterOptions<UserOptionType>();
 
@@ -10,7 +12,11 @@ interface UserOptionType {
   id?: number;
 }
 
-export default function UserSelector(props: { disabled: boolean, userList: UserOptionType[], setUserName: (userName: string) => void }) {
+export default function UserSelector(props: {
+  disabled: boolean;
+  userList: UserOptionType[];
+  setUserName: (userName: string) => void;
+}) {
   return (
     <Autocomplete
       freeSolo
@@ -18,22 +24,32 @@ export default function UserSelector(props: { disabled: boolean, userList: UserO
       id="username-selector"
       options={props.userList}
       onChange={(event, userName) => {
-        if (typeof userName === 'string') {
+        if (typeof userName === "string") {
           props.setUserName(userName);
         } else if (userName && userName.inputValue) {
-          props.setUserName(userName.inputValue)
+          props.setUserName(userName.inputValue);
         } else if (userName && userName.name) {
-          props.setUserName(userName.name)
+          props.setUserName(userName.name);
         }
       }}
       style={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Choose User" variant="outlined" required />}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose User"
+          variant="outlined"
+          required
+        />
+      )}
       disabled={props.disabled}
       filterOptions={(options, params) => {
         const filtered = filter(options, params);
 
         // Suggest the creation of a new value
-        if (params.inputValue !== '') {
+        if (
+          params.inputValue !== "" &&
+          !(filtered.length == 1 && filtered[0].name == params.inputValue)
+        ) {
           filtered.push({
             inputValue: params.inputValue,
             name: `Add "${params.inputValue}"`,
@@ -44,7 +60,7 @@ export default function UserSelector(props: { disabled: boolean, userList: UserO
       }}
       getOptionLabel={(option) => {
         // Value selected with enter, right from the input
-        if (typeof option === 'string') {
+        if (typeof option === "string") {
           return option;
         }
         // Add "xxx" option created dynamically
@@ -54,6 +70,7 @@ export default function UserSelector(props: { disabled: boolean, userList: UserO
         // Regular option
         return option.name;
       }}
+      renderOption={(option) => option.name}
     />
   );
 }
